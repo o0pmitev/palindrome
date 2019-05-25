b5196a2dffb1d9a24daf62d1f7fd30aeac691cc9
Plamen Mitev <pmitev89@gmail.com>
2019-05-25 11:51:18 +0200

    Initialize repository

---
+++ index.js
@@ -0,0 +1,51 @@
+// Reverse a string
+function reverse(string) {
+  return Array.from(string).reverse().join("");
+}
+
+// Return Username and Domain name
+function emailParts(email) {
+  return  email.split("@");
+}
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+// Define a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Return a LOUDER version of the content.
+  this.louder = function() {
+    let louder = this.content.toUpperCase();
+    return louder;
+  }
+  // Return lower case string for palindrome testing
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+  // Return content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Return true for a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Return translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
